0789a1712a266aa96ab2689fb1032c1807f5e4ca
sheylao <sheypr85@gmail.com>
2018-11-18 00:32:39 -0600

    Adding changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
     $('.timepicker').timepicker({twelveHour: false, autoClose: true,});
+    
   });
          
   var ct = moment()
@@ -29,34 +30,40 @@ $("#button_submit").on("click", function (event) {
     // Don't refresh the page!
     event.preventDefault();
 
+
     trainName = $("#train_name").val().trim();
     destination = $("#destination_name").val().trim();
     trainStart = $("#start_input").val().trim();
     frequency = $("#frequency_input").val().trim();
 
-    var trainInfo = {
-        trainName: trainName,
-        destination: destination,
-        trainStart: trainStart,
-        frequency: frequency
-    };
-    database.ref().push(trainInfo);
 
-    console.log(trainInfo.trainName);
-    console.log(trainInfo.destination);
-    console.log(trainInfo.trainStart);
-    console.log(trainInfo.frequency);
+    if (!(trainName == '' || destination == '' || trainStart == '' || frequency == '')) {
 
-
-    $("#train_name").val("");
-    $("#destination_name").val("");
-    $("#start_input").val("");
-    $("#frequency_input").val("");
-
+        var trainInfo = {
+            trainName: trainName,
+            destination: destination,
+            trainStart: trainStart,
+            frequency: frequency
+        };
+    
+    
+        database.ref().push(trainInfo);
+    
+    
+        $("#train_name").val("");
+        $("#destination_name").val("");
+        $("#start_input").val("");
+        $("#frequency_input").val("");
+    
+        } else {
+         alert("Please Fill All Fields.");
+        }
+        
+       
 });
 
 database.ref().on("child_added", function(childSnapshot) {
-    console.log(childSnapshot.val());
+
   
     // Store everything into a variable.
     var trainName = childSnapshot.val().trainName;
@@ -65,36 +72,28 @@ database.ref().on("child_added", function(childSnapshot) {
     var frequency = childSnapshot.val().frequency;
   
 
-    // Assumptions
     var tFrequency = frequency;
 
-    // Time is 3:30 AM
+    // Time is set by user
     var firstTime = trainStart;
 
     // First Time (pushed back 1 year to make sure it comes before current time)
     var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
-    console.log(firstTimeConverted);
-
-    // Current Time
-    var currentTime = moment();
-    console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
 
     // Difference between the times
     var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
-    console.log("DIFFERENCE IN TIME: " + diffTime);
+
 
     // Time apart (remainder)
     var tRemainder = diffTime % tFrequency;
-    console.log(tRemainder);
+    
 
     // Minute Until Train
     var tMinutesTillTrain = tFrequency - tRemainder;
-    console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+    
 
     // Next Train
     var nextTrain = moment().add(tMinutesTillTrain, "minutes");
-    console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
-
 
 
 
@@ -109,7 +108,5 @@ database.ref().on("child_added", function(childSnapshot) {
   
     // Append the new row to the table
     $("#table_body").append(newRow);
-
-    ref.remove()
 
   });
